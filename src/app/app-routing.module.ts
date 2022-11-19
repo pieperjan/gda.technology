98e80987787d0f941eaa725d246efa405fea7b02
Jan Pieper <hallo@pieperjan.de>
2022-11-19 13:03:06 +0100

    - V 1.0.1 ist fertiggestellt und Online

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { VisionComponent } from './_component/vision/vision.component';
 import { AboutComponent } from './_component/about/about.component';
 import { ProjekteComponent } from './_component/projekte/projekte.component';
 import { HomeComponent } from './_component/home/home.component';
+import { ImpressumComponent } from './_component/impressum/impressum.component';
+import { DatenschutzComponent } from './_component/datenschutz/datenschutz.component';
 import { FormateComponent } from './_atome/formate/formate.component';
 
 const routes: Routes = [
@@ -15,6 +17,8 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'arbeiten', component: ProjekteComponent },
   { path: 'formate', component: FormateComponent },
+  { path: 'impressum', component: ImpressumComponent },
+  { path: 'datenschutz', component: DatenschutzComponent },
 ];
 
 @NgModule({
